4818de12a889f15bf36eae831b7c55e113a2b7bb
Yuvraj Rajeev Singh <ys3334@nyu.edu>
2020-06-19 01:22:38 -0400

    Adding AccountManager Microservice

--- account-manager/src/main/java/com/example/accountmanager/AccountManagerApplication.java
+++ account-manager/src/main/java/com/example/accountmanager/AccountManagerApplication.java
@@ -1,9 +1,13 @@
-package com.example.accountmanager.accountmanager;
+package com.example.accountmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//This file must be hierarchial above the controller as this will begin the classpath, and leads to error if it is not
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountManagerApplication {
 
     public static void main(String[] args) {
--- account-manager/src/test/java/com/example/accountmanager/AccountManagerApplicationTests.java
+++ account-manager/src/test/java/com/example/accountmanager/AccountManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.accountmanager.accountmanager;
+package com.example.accountmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
