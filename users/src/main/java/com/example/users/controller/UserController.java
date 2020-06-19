860a24596114c4469c42c9f7e74a6c1673540d5c
Yuvraj Rajeev Singh <ys3334@nyu.edu>
2020-06-19 00:43:47 -0400

    Adding a new microservices

---
+++ users/src/main/java/com/example/users/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/status/check")
+    public String status()
+    {
+        return "Working";
+    }
+}
